cc785563fb865ca8f6856c4da753b2f5a0210217
Andrey Chernoprudov <dinloq@gmail.com>
2021-06-08 21:13:41 +0500

    init commit

---
+++ MicroApp/MicroApp.h
@@ -0,0 +1,18 @@
+//
+//  MicroApp.h
+//  MicroApp
+//
+//  Created by Andrey Chernoprudov on 08.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MicroApp.
+FOUNDATION_EXPORT double MicroAppVersionNumber;
+
+//! Project version string for MicroApp.
+FOUNDATION_EXPORT const unsigned char MicroAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicroApp/PublicHeader.h>
+
+
