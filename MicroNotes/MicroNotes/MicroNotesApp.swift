2f619e18260a90640325d02379fe3b8c55dd20d5
Andrey Chernoprudov <dinloq@gmail.com>
2021-06-08 21:17:45 +0500

    MicroNotes app init

---
+++ MicroNotes/MicroNotes/MicroNotesApp.swift
@@ -0,0 +1,20 @@
+//
+//  MicroNotesApp.swift
+//  MicroNotes
+//
+//  Created by Andrey Chernoprudov on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MicroNotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
