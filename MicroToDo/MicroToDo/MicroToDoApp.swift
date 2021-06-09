b6f8040a640310612f8c55d5fae56d079e2df385
Andrey Chernoprudov <dinloq@gmail.com>
2021-06-09 11:36:58 +0500

    Micro ToDo App init

---
+++ MicroToDo/MicroToDo/MicroToDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MicroToDoApp.swift
+//  MicroToDo
+//
+//  Created by Andrey Chernoprudov on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MicroToDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
