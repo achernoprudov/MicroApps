f0a6954be06fea56ef55c089ffa5d3d4c6acb1cd
Andrey Chernoprudov <dinloq@gmail.com>
2021-07-21 18:18:46 +0500

    WatchApp init

---
+++ MicroToDo/WatchApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchApp Extension
+//
+//  Created by Andrey Chernoprudov on 21.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MicroToDo/WatchApp Extension/MicroToDoApp.swift
@@ -0,0 +1,19 @@
+//
+//  MicroToDoApp.swift
+//  WatchApp Extension
+//
+//  Created by Andrey Chernoprudov on 21.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MicroToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
