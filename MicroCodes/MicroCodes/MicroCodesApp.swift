b67e3c34dd816a8bacf66e19d405409cbf348073
Andrey Chernoprudov <dinloq@gmail.com>
2021-07-21 18:32:24 +0500

    App identity

--- MicroCodes/MicroCodes/MicroCodesApp.swift
+++ MicroCodes/MicroCodes/MicroCodesApp.swift
@@ -16,6 +16,7 @@ struct MicroCodesApp: App {
             NavigationView {
                 ContentView()
             }
+            .accentColor(.purple)
             .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
--- MicroCodes/UI/ContentView.swift
+++ MicroCodes/UI/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
         }
         .listStyle(GroupedListStyle())
         .navigationBarItems(trailing: EditButton())
+        .navigationTitle("MicroCodes")
         .toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
                 Spacer()
